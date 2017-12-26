6c955947556285e2e195f7733f2c0292e88d8f0f
pRusya <agat.nk@gmail.com>
2017-12-26 17:46:15 +0200

    Listen to onCreated because onUpdated does not work

--- background.js
+++ background.js
@@ -12,3 +12,23 @@ chrome.tabs.onUpdated.addListener(function (id, obj, tab) {
     }
   });
 })
+
+chrome.tabs.onCreated.addListener(function (tab) {
+  setTimeout(function() {
+    chrome.tabs.get(tab.id, function(waitedTab){
+      if (waitedTab.url === undefined || waitedTab.url === "") {
+        return
+      }
+      chrome.storage.sync.get("pattern", function (store) {
+        if (store.pattern === undefined || store.pattern === "") {
+          return
+        }
+        var re = new RegExp(store.pattern, "i");
+        if (re.test(waitedTab.url)) {
+          chrome.tabs.remove(waitedTab.id, function () {});
+        }
+      });
+    })
+  }, 500)
+})
+
